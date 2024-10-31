5b76e9d5b3fa488e3fb7eb9ae4eec7e5387cabdc
Harun Demirpençe <harundemirpence@Haruns-MacBook-Air.local>
2024-10-31 22:25:04 +0300

    add Users and Subscription tables

---
+++ users_subscription.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Users(
+    user_id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    subscription_id INT NOT NULL DEFAULT 0,
+    PRIMARY KEY user_id,
+    FOREIGN KEY(subscription_id) 
+        REFERENCES Subscriptions(subscription_id)
+        ON DELETE DEFAULT 
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Subscriptions(
+    subscription_id INT AUTO_INCREMENT PRIMARY KEY,
+    subscription VARCHAR(30) NOT NULL UNIQUE
+);
