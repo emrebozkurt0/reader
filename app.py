b9c017531ddd748fc5a6999a54fb5afdcc60fde9
emrebozkurt0 <emrebozkurt117@gmail.com>
2024-11-17 22:33:38 +0300

    connect routes to app

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+from main.routes import *
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
--- main/routes.py
+++ main/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, request
-from .models import fetch_data, insert_data, execute_query_from_file
-from ..app import app
+from app import app
 
 @app.route('/')
 def get_users():
-    return "First view"
+    return render_template("home.html")
