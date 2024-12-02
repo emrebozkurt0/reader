476ce3f6fc0aa0adb4e11ab799c57bfdd9cfd0ad
mberkayarsln <mberkayarslann@gmail.com>
2024-12-02 22:39:11 +0300

    create admin panel

--- main/routes.py
+++ main/routes.py
@@ -2,5 +2,9 @@ from flask import render_template, request
 from app import app
 
 @app.route('/')
-def get_users():
-    return render_template("home.html",content="Welcome")
+def home():
+    return render_template("home.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("admin.html")
