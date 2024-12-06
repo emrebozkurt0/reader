e9286ecc5cb621d71e6667533ba2fac753da8da1
mberkayarsln <mberkayarslann@gmail.com>
2024-12-06 21:48:18 +0300

    crud pages

--- main/routes.py
+++ main/routes.py
@@ -1,10 +1,31 @@
 from flask import render_template, request
 from app import app
 
-@app.route('/')
+""" @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html") """
 
-@app.route('/admin')
+
+@app.route("/")
 def admin():
     return render_template("admin.html")
+
+
+@app.route("/books")
+def books():
+    return render_template("/crud/books.html")
+
+
+@app.route("/users")
+def users():
+    return render_template("/crud/users.html")
+
+
+@app.route("/publishers")
+def publishers():
+    return render_template("/crud/publishers.html")
+
+
+@app.route("/authors")
+def authors():
+    return render_template("/crud/authors.html")
