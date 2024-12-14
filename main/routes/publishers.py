96fc31fa7e1de8b0cb065e8a1065068b05ac0773
mberkayarsln <mberkayarslann@gmail.com>
2024-12-14 20:59:52 +0300

    publisher update and delete

--- main/routes/publishers.py
+++ main/routes/publishers.py
@@ -3,12 +3,14 @@ from main.classes.publishers import Publishers
 from main.utils.get_data import get_table_data
 from main.utils.database import get_connection
 
-publishers_bp = Blueprint('publishers', __name__)
+publishers_bp = Blueprint("publishers", __name__)
+
 
 @publishers_bp.route("/publishers")
 def publishers():
     publishers = get_table_data("Publishers")
     return render_template("/crud/publishers/publishers.html", publishers=publishers)
+
 
 @publishers_bp.route("/publishers/add", methods=["GET", "POST"])
 def add_publisher():
@@ -24,3 +26,39 @@ def add_publisher():
         except Exception as e:
             return f"Error occurred while adding the publisher: {e}", 500
     return render_template("/crud/publishers/add_publisher.html")
+
+
+@publishers_bp.route("/publishers/update/<int:id>", methods=["GET", "POST"])
+def update_publisher(id):
+    if request.method == "POST":
+        data = {
+            "publisher_name": request.form["name"],
+        }
+        try:
+            connection = get_connection()
+            publisher = Publishers(connection)
+            publisher.update(data,id)
+            return redirect(url_for("publishers.publishers"))
+        except Exception as e:
+            return f"Error occurred while updating the publisher: {e}", 500
+    else:
+        try:
+            connection = get_connection()
+            publisher = Publishers(connection)
+            publisher_data = publisher.get_by_id(id)
+            return render_template(
+                "/crud/publishers/update_publisher.html", publisher=publisher_data
+            )
+        except Exception as e:
+            return f"Error occurred while fetching the publisher data: {e}", 500
+
+
+@publishers_bp.route("/publishers/delete/<int:id>", methods=["POST"])
+def delete_publisher(id):
+    try:
+        connection = get_connection()
+        publisher = Publishers(connection)
+        publisher.delete(id)
+        return redirect(url_for("publishers.publishers"))
+    except Exception as e:
+        return f"Error occurred while deleting the publisher: {e}", 500
